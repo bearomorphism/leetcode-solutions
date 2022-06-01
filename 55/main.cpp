8261a000e6a7db697e264231f3646b8bf3d21007
bearomorphism <bear890707@gmail.com>
2022-06-01 14:05:27 +0800

    55

---
+++ 55/main.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int m = 0, i = 0;
+        while (i <= m)
+        {
+            m = max(m, i + nums[i]);
+            i++;
+            if (m >= nums.size() - 1)
+                return true;
+        }
+        return false;
+    }
+};
