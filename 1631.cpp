89c1454370a556ce04e63f2bd4969dcb98e12242
Yu-Ting Hsiung <bear890707@gmail.com>
2022-09-01 11:44:48 +0800

    1631

---
+++ 1631.cpp
@@ -0,0 +1,30 @@
+typedef pair<int, pair<int, int>> iPair;
+
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int row = heights.size(), col = heights[0].size();
+        priority_queue<iPair, vector<iPair>, greater<iPair>> pq;
+        vector<vector<bool>> vis(row, vector<bool>(col, false));
+        pq.push({0, {0, 0}});
+        const int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+        int ans = 0;
+        while (!pq.empty()) {
+            auto p = pq.top();
+            pq.pop();
+            int x = p.second.first, y = p.second.second;
+            if (!vis[x][y]) {
+                vis[x][y] = true;
+                ans = max(ans, p.first);
+                if (x == row - 1 && y == col - 1) return ans;
+                for (int d = 0; d < 4; d++) {
+                    int a = x + dx[d], b = y + dy[d];
+                    if (0 <= min(a, b) && a < row && b < col && !vis[a][b]) {
+                        pq.push({abs(heights[a][b] - heights[x][y]), {a, b}});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
