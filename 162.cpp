459cbb680b48cb2670438b34ce9a711892e53613
Yu-Ting Hsiung <bear890707@gmail.com>
2022-09-03 13:24:33 +0800

    162

---
+++ 162.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0, hi = n - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return hi;
+    }
+};
