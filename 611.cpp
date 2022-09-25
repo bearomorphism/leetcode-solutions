12e717f885baf4b3c893cae3adef978ca7dcc771
Yu-Ting Hsiung <bear890707@gmail.com>
2022-09-25 20:36:05 +0800

    611 improve

--- 611.cpp
+++ 611.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int triangleNumber(vector<int>& nums) {
-        vector<int> v;
-        for (int i: nums) {
-            if (i > 0) v.push_back(i);
-        }
+    int triangleNumber(vector<int>& v) {
         sort(v.begin(), v.end());
         int n = v.size();
-        int result = n * (n - 1) * (n - 2) / 6;
+        int result = 0;
         for (int i = 0; i < n; i++) {
+            int k = i + 2;
             for (int j = i + 1; j < n; j++) {
-                int target = v[i] + v[j];
-                int x = v.end() - lower_bound(v.begin(), v.end(), target);
-                cout << x << '\n';
-                result -= x;
+                k = max(k, j + 1);
+                int tmp = v[i] + v[j];
+                while (k < n && tmp > v[k]) k++;
+                result += k - j;
             }
         }
         return result;
